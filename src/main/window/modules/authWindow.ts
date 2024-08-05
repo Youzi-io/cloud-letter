4d8144e3e9c442e7a1154e7a5566c2d0880f556c
Yuzu <1581523609@qq.com>
2024-08-05 22:03:10 +0800

    fix：修复可拖动区域右键触发系统菜单问题

--- src/main/window/modules/authWindow.ts
+++ src/main/window/modules/authWindow.ts
@@ -12,6 +12,7 @@ export default class AuthWindow extends CommonWindow {
       show: false,
       frame: false, // 无边框
       resizable: false, // 不允许改变窗口大小
+      maximizable: false, // 不允许最大化
       ...(process.platform === 'linux' ? { icon } : {}),
       webPreferences: {
         // Use pluginOptions.nodeIntegration, leave this alone
@@ -25,5 +26,16 @@ export default class AuthWindow extends CommonWindow {
       windowType: ElectronWindowType.Auth,
       loadUrl: (host) => `${host}#auth`
     })
+    const win = this.getWindow()
+    if (win) {
+      // 解决可拖动区域右键触发菜单问题（0x0116为window消息）
+      win.hookWindowMessage(0x0116, () => {
+        win.setEnabled(false)
+        setTimeout(() => {
+          win.setEnabled(true)
+        }, 0)
+        return true
+      })
+    }
   }
 }
--- src/main/window/modules/mainWindow.ts
+++ src/main/window/modules/mainWindow.ts
@@ -26,5 +26,16 @@ export default class MainWindow extends CommonWindow {
       windowType: ElectronWindowType.Main,
       loadUrl: (host) => `${host}#main`
     })
+    const win = this.getWindow()
+    if (win) {
+      // 解决可拖动区域右键触发菜单问题
+      win.hookWindowMessage(0x0116, () => {
+        win.setEnabled(false)
+        setTimeout(() => {
+          win.setEnabled(true)
+        }, 0)
+        return true
+      })
+    }
   }
 }
